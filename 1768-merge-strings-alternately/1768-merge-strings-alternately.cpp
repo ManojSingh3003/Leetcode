38ab83f26666c38924fc2da16a8bc16ce245aea5
Manbendra Singh <manbendrasingh3003@gmail.com>
2026-07-03 20:39:06 +0530

    Time: 4 ms (31.73%), Space: 8.3 MB (97.59%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1=word1.size();
+        int n2=word2.size();
+
+        string ans="";
+        int i=0,j=0;
+        while(i<n1 || j<n2){
+            if(i<n1){
+                ans+=word1[i++];
+            }
+            if(j<n2){
+                ans+=word2[j++];
+            }
+        }
+        return ans;
+    }
+};
+//TC:max(n1,n2);
+//SC:n1+n2;
