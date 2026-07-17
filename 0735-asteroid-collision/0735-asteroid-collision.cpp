eb018d37ba51b220d4ef593781dd46c2511d5b01
Manbendra Singh <manbendrasingh3003@gmail.com>
2026-07-17 11:12:23 +0530

    Time: 2 ms (33.55%), Space: 22.2 MB (19.34%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n=a.size();
+        stack<int> st;
+
+        for(int i=0;i<n;i++){
+            int x=a[i];
+            bool alive=1;
+
+            
+            while(!st.empty() && st.top()>0 && x<0){
+                if(abs(x)==abs(st.top())){
+                    st.pop();
+                    alive=false;
+                    break;
+                }else if(abs(x)>abs(st.top())){
+                    st.pop();
+                }else {
+                    alive=false;
+                    break;
+                }
+            }
+            if(alive)st.push(x);
+        }
+        
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
