bd17a299ea01f97778bcfb5da2aab76f24987860
Manbendra Singh <manbendrasingh3003@gmail.com>
2026-09-16 20:10:55 +0530

    Time: 798 ms (16.82%), Space: 290.8 MB (6.07%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,29 +1,31 @@
 class Solution {
 const int mod=1e9+7;
-vector<vector<int>> memo;
+vector<vector<vector<int>>> memo;
 private:
-    int dp(int i,int k,int& n){
+    int dp(int i,int k,int on,int n){
         if(i==n){
             if(k==0)return 1;
             else return 0;
         }
         if(k==0)return 1;
 
-        if(memo[i][k]!=-1)return memo[i][k];
+        if(memo[i][k][on]!=-1)return memo[i][k][on];
 
-        int ans=dp(i+1,k,n);
-    
-        for(int j=i+1;j<n;j++){
-            ans = (ans + dp(j,k-1,n) )%mod;
-            if(n-j-1 < k-1 ) break;
+        int ans=0;
+        if(on){
+            ans = (ans + dp(i+1, k, 1, n)) % mod;
+            ans = (ans + dp(i, k-1, 0, n)) % mod;
+        }else{
+            ans = (ans + dp(i+1, k, 0, n)) % mod;
+            ans = (ans + dp(i+1,k,1,n)) % mod;
         }
-        return memo[i][k]=ans;
+        return memo[i][k][on]=ans;
     }
 public:
     int numberOfSets(int n, int k) {
-        memo.assign(n+1,vector<int>(k+1,-1));
+        memo.assign(n+1,vector<vector<int>>(k+1,vector<int>(2,-1)));
 
-        int ans=dp(0,k,n);
+        int ans=dp(0,k,0,n);
         return ans;
     }
 };
